d4c948bcd67e22d27dde38d1373f80e2f0d382f9
Jason Rosenman <jvrosenman@gmail.com>
2020-06-24 21:04:28 -0400

    Fixed some comments and the WMMV8 tapdance

--- keyboards/boston_meetup/2019/keymaps/oscillope/keymap.c
+++ keyboards/boston_meetup/2019/keymaps/oscillope/keymap.c
@@ -67,6 +67,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [WMMV5] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_wm_mv_finished, td_wm_mv_reset),
   [WMMV6] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_wm_mv_finished, td_wm_mv_reset),
   [WMMV7] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_wm_mv_finished, td_wm_mv_reset),
+  [WMMV8] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_wm_mv_finished, td_wm_mv_reset),
   [WMMV9] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_wm_mv_finished, td_wm_mv_reset),
 };
 
@@ -123,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,------.
- * | Esc  | 
+ * | Next | 
  * |------+------+-------------.
  * |RGB TG|RGB M+|RGB M-|      |
  * |------+------+------+------|
@@ -142,13 +143,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust
  * ,------.
- * | DFU  | 
+ * | Next | 
  * |------+------+-------------.
  * |HPT TG|HPT FB|HPT RS| BKSP | 
  * |------+------+------+------|
  * |      |HPT M+|      |      | 
  * |------+------+------+------|
- * |      |HPT M-|Clk TG| Del  |
+ * | DFU  |HPT M-|Clk TG| Del  |
  * `---------------------------'
  */
 [_ADJUST] = LAYOUT(
@@ -170,7 +171,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------'
  */
 [_NUM] = LAYOUT(
-  TG(_NUM) ,
+  TO(_BASE),
   XXXXXXX, KC_7   , KC_8   , KC_9   ,
   XXXXXXX, KC_4   , KC_5   , KC_6   ,
   KC_0   , KC_1   , KC_2   , KC_3
