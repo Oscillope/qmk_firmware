90f91dfcedca175cbdb6c57501c657a4410c42c2
Oscillope <hello@oscillope.xyz>
2023-05-01 11:19:01 -0400

    Update gaming layer

--- keyboards/orthodox/keymaps/oscillope/keymap.c
+++ keyboards/orthodox/keymaps/oscillope/keymap.c
@@ -55,9 +55,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_GAME]  = LAYOUT(
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_C,    KC_V,             _______, TG(_GAME),        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-    KC_Z,    KC_A,    KC_S,    KC_D,    KC_F,    KC_X,    KC_SPC,  _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                                   KC_6,    _______, _______, _______, _______,    _______,
+    _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_C,    KC_V,             _______, TG(_GAME),        _______, _______, _______, _______, _______, _______,
+    KC_Z,    KC_A,    KC_S,    KC_D,    KC_F,    KC_X,    KC_SPC,  _______, _______,          _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   [_LOWER] = LAYOUT(
